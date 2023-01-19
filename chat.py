c9613719643037bb57129c5257ab0b11530de264
ori <63188591+OriCat101@users.noreply.github.com>
2023-01-19 22:08:13 +0100

    converted code to functions

--- chat.py
+++ chat.py
@@ -1,19 +1,30 @@
 import socket
 import threading
 
-choice = input("Do you want to host (1) or to connect (2): ")
 
-if choice == "1":
+def listen(ip, port):
+    """
+    Listens on the specified port for an incoming connection.
+    IP is optional
+    """
+    if ip == "":
+        ip = "0.0.0.0"
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("0.0.0.0", 9999))
+    server.bind((ip, port))
     server.listen()
 
-    client, _ = server.accept()
-elif choice == "2":
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("localhost", 9999))
-else:
-    exit
+    # client, _ = server.accept()
+    return server.accept()
+
+
+def connect(ip, port):
+    """
+    Connects to another client using the given port and IP.
+    """
+    # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((ip, port))
+
 
 def sending_messages(c):
     while True:
@@ -24,10 +35,18 @@ def sending_messages(c):
 
 def receiving_messages(c):
     while True:
-        print("Partner: " + c.recv(1024).decode())
+        data = c.recv(1024).decode()
+        if data != "":
+            print("Partner: " + data)
 
+
+choice = input("Do you want to host (1) or to connect (2): ")
+if choice == "1":
+    client, _ = listen("0.0.0.0", 9999)
+
+elif choice == "2":
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connect("localhost", 9999)
 
 threading.Thread(target=sending_messages, args=(client,)).start()
 threading.Thread(target=receiving_messages, args=(client,)).start()
-
-
