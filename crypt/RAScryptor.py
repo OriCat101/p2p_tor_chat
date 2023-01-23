75f54775ecf34be5e6def50c8ed27f9ce285247c
kayihandevin-bzz <redbadcommander@pm.me>
2023-01-23 14:04:01 +0100

    added doc for RSAcryptor

--- crypt/RAScryptor.py
+++ crypt/RAScryptor.py
@@ -1,9 +1,14 @@
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
 
 
 def load_public_key(public_key):
-    # Load the recipient's public key
+    """
+    Loads a public key from the specified file in PEM format.
+    :param public_key: The file path of the public key
+    :return: A public key object
+    """
     with open(public_key, "rb") as key_file:
         recipient_public_key = serialization.load_pem_public_key(
             key_file.read(),
@@ -12,8 +17,13 @@ def load_public_key(public_key):
     return recipient_public_key
 
 
-def encrypt_message(recipient_public_key):
-    # Encrypt the message
+def encrypt_message(recipient_public_key, message):
+    """
+    Encrypts the message using the recipient's public key.
+    :param recipient_public_key: The recipient's public key
+    :param message: The message to be encrypted
+    :return: The encrypted message
+    """
     ciphertext = recipient_public_key.encrypt(
         message.encode(),
         padding.OAEP(
@@ -26,7 +36,11 @@ def encrypt_message(recipient_public_key):
 
 
 def load_private_key(private_key):
-    # Load the recipient's private key
+    """
+    Loads a private key from the specified file in PEM format.
+    :param private_key: The file path of the private key
+    :return: A private key object
+    """
     with open(private_key, "rb") as key_file:
         recipient_private_key = serialization.load_pem_private_key(
             key_file.read(),
@@ -36,8 +50,13 @@ def load_private_key(private_key):
     return recipient_private_key
 
 
-def decrypt_message(recipient_private_key)
-    # Decrypt the message
+def decrypt_message(recipient_private_key, ciphertext):
+    """
+    Decrypts the ciphertext using the recipient's private key.
+    :param recipient_private_key: The recipient's private key
+    :param ciphertext: The encrypted message
+    :return: The decrypted message
+    """
     plaintext = recipient_private_key.decrypt(
         ciphertext,
         padding.OAEP(
@@ -47,3 +66,4 @@ def decrypt_message(recipient_private_key)
         )
     )
     return plaintext
+
